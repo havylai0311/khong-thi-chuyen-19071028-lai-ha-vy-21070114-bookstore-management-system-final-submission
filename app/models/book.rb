27e71bc93ad7cf664ae59d4cc1822c618a24ee6d
Your Name <you@example.com>
2023-05-17 08:58:46 +0700

    Add model validation and association and update route url

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author
   validates_presence_of :book_name, :description, :status, :price
-  validetes_uniqueness_of :book_name
+  validates_uniqueness_of :book_name
 end
