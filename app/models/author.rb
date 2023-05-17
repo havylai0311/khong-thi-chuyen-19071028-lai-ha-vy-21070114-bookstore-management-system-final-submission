b8249f0e7c4834c235abda2425d6d0a2acbed1da
Your Name <you@example.com>
2023-05-17 23:55:01 +0700

    Polishing plate boiler content

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
+has_many :books
 validates_presence_of :author_name
 validates_uniqueness_of :author_name
+validates :author_name, presence: true
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :author
+belongs_to :author, class_name:'Author', foreign_key: 'author_id'
   validates_presence_of :book_name, :description, :status, :price
   validates_uniqueness_of :book_name
 end
